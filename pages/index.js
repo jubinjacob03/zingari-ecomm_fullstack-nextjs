c50ab04d33865a88bbd356d1ac92d26d9c0ceffc
jubinjacob03 <jr.jubin03@gmail.com>
2024-06-02 00:37:29 +0530

    Added Collection & Featured ID to env

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,13 @@ import { Product } from "@/models/Product";
 import Products from "./components/Products";
 import Collection from "./components/Collection";
 
-export default function Home({ featuredProduct, newProducts, collectionProduct, allProducts }) {
+export default function Home({
+  featuredProduct,
+  newProducts,
+  collectionProduct,
+}) {
   return (
-    <main
-      className={`min-h-screen p-6 bg-background `}
-    >
-
+    <main className={`min-h-screen p-6 bg-background `}>
       <Hero product={featuredProduct} />
 
       <hr class="my-1 h-px border-0 bg-gray-300" />
@@ -18,25 +19,26 @@ export default function Home({ featuredProduct, newProducts, collectionProduct, 
       <hr class="my-1 h-px border-0 bg-gray-300" />
       <Collection product={collectionProduct} />
     </main>
-  )
+  );
 }
 
 export async function getServerSideProps() {
   await mongooseConnect();
-  const featuredId = '6644fef50f5ea948d9f9cad7';
-  const collectionId = '6648d996dae1c9b2b0d9ec55';
+  const featuredId = process.env.FEATURED_PRODUCT_ID;
+  const collectionId = process.env.COLLECTION_PRODUCT_ID;
 
   const featuredProduct = await Product.findById(featuredId);
   const collectionProduct = await Product.findById(collectionId);
-  const newProducts = await Product.find({}, null, {sort: {'_id': 1}, limit: 5})
-  const allProducts = await Product.find({}, null, {sort: {'_id': 1}})
+  const newProducts = await Product.find({}, null, {
+    sort: { _id: -1 },
+    limit: 5,
+  });
 
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
       collectionProduct: JSON.parse(JSON.stringify(collectionProduct)),
       newProducts: JSON.parse(JSON.stringify(newProducts)),
-      allProducts: JSON.parse(JSON.stringify(allProducts)),
-    }
-  }
+    },
+  };
 }
